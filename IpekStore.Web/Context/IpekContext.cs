a9da2c58a81b884beb0baf18517bfaff0d5e0715
= <=>
2021-05-06 17:57:30 +0300

    ipekstore dosyalarının ilk yüklemesi

---
+++ IpekStore.Web/Context/IpekContext.cs
@@ -0,0 +1,38 @@
+﻿using IpekStore.Web.DbMappings;
+using IpekStore.Web.Models.Entities;
+using IpekStore.Web.Seeder;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IpekStore.Web.Context
+{
+    public class IpekContext:DbContext
+    {
+        public IpekContext(DbContextOptions opt)
+            :base(opt)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //CategoryMapping
+            modelBuilder.ApplyConfiguration(new CategoryMapping());
+            //ProductMapping
+            modelBuilder.ApplyConfiguration(new ProductMapping());
+
+            //Category Data Seeding
+            modelBuilder.SeedCategories();
+            //Product Data Seeding
+            modelBuilder.SeedProducts();
+        }
+
+    }
+}
---
+++ IpekStore.Web/DbMappings/ProductMapping.cs
@@ -0,0 +1,36 @@
+﻿using IpekStore.Web.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IpekStore.Web.DbMappings
+{
+    public class ProductMapping : AuditableEntityMapping<Product>
+    {
+        public override void Configure(EntityTypeBuilder<Product> builder)
+        {
+            //Product sınıfının miras aldığı Auditable Entity türünden
+            //gelecek Property'ler veritabanına map leniyor.
+            base.Configure(builder);
+
+
+            //Product Entity'nin kendisine ait Propertyler map leniyor.            
+            builder
+                .Property(p => p.Name)
+                .HasColumnType("varchar(150)")
+                .IsRequired();
+
+            //Birden çoğa ilişki kuruluyor
+            builder
+                .HasOne(p => p.Category)
+                .WithMany(c => c.Products)
+                .HasForeignKey(p => p.CategoryId);
+
+            builder
+                .ToTable("Products");
+        }
+    }
+}
